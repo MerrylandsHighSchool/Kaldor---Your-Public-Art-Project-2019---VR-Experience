98c7073810360df89b043fcf703c1ee7da6a62cc
Velden Bergonia <velden.bergonia@education.nsw.gov.au>
2019-10-28 12:03:46 +1100

    added map material and object

---
+++ Assets/Scripts/ChangeWorld.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeWorld : MonoBehaviour
+{
+    public string TargetScene;
+
+    public void NewScene ()
+    {
+        SceneManager.LoadScene(TargetScene, LoadSceneMode.Single);
+    }
+}
